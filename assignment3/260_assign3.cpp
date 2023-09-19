0a18cf8d7cc734e31ffeb9802b39ca0f0a084eb7
CollinJH <cjchargreaves@gmail.com>
2023-09-19 15:40:49 -0700

    update

--- assignment3/260_assign3.cpp
+++ assignment3/260_assign3.cpp
@@ -40,10 +40,35 @@ int main()
 
 
 string addbin(string bin1, string bin2)
-{ 
-  
+{
 
-	
+  // iterate over each subsequent string with two variables
+  // check conditions
+  // if 0 + 0 result = 0
+  // if 0 + 1 result = 1
+  // if 1 + 0 result = 1
+  // if 1 + 1 result = 10, carry 1 to next highest bit
+  string bin_add;
+  int length = bin1.length() - 1;
+  int j = 0;
+
+  for (int i = 0; i < length; i++) {
+    if (bin1[i] == '0' && bin2[j] == '0') {
+      bin_add += '0';
+    } else if (bin1[i] == '1' && bin2[j] == '0') {
+      bin_add += '1';
+    } else if (bin1[i] == '0' && bin2[j] == '1') {
+      bin_add += '1';
+    } else if (bin1[i] == '1' && bin2[j] == '1') {
+      for (int k = 0; k < bin_add; k++) {
+
+      }
+    }
+  }
+
+
+
+	return bin_add;
 }
 
 string addhex(string hex1, string hex2)
